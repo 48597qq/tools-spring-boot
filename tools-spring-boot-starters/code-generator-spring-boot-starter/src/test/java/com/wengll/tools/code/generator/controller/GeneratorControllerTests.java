3303f650815c417fa05c11518cf1f0174e7dbb28
48597qq <371541074@qq.com>
2020-05-18 18:13:32 +0800

    * 增加测试单元

---
+++ tools-spring-boot-starters/code-generator-spring-boot-starter/src/test/java/com/wengll/tools/code/generator/controller/GeneratorControllerTests.java
@@ -0,0 +1,35 @@
+package com.wengll.tools.code.generator.controller;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeneratorControllerTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    public void testDownload() throws Exception {
+    }
+}
