32e7b314e8b8bec38c5ecb7a6005a861d6972707
48597qq <371541074@qq.com>
2020-05-22 17:39:24 +0800

    * 修改代码

---
+++ tools-spring-boot-starters/code-generator-spring-boot-starter/src/main/java/org/wengll/tools/code/generator/config/CodeGeneratorWebMvcConfig.java
@@ -0,0 +1,14 @@
+package org.wengll.tools.code.generator.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CodeGeneratorWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
--- tools-spring-boot-starters/code-generator-spring-boot-starter/src/main/resources/templates/generator/list.ftl
+++ tools-spring-boot-starters/code-generator-spring-boot-starter/src/main/resources/templates/generator/list.ftl
@@ -1,1 +1,19 @@
-<h2>代码生成器测试页面1</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>代码生成器页面</title>
+    <link rel="stylesheet" href="/webjars/bootstrap/3.4.1/css/bootstrap.min.css" />
+    <script src="/webjars/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/webjars/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container"><br/>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            代码生成器
+        </div>
+    </nav>
+</div>
+</body>
+</html>
